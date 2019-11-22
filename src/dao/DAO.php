826576b9c337da70c350718b6d37581be7570274
Dev0108 <laurent.duquesnoy@student.howest.be>
2019-11-22 11:05:34 +0100

    last commit before consult sprint 2

--- src/dao/DAO.php
+++ src/dao/DAO.php
@@ -1,31 +1,30 @@
 <?php
 
-class DAO {
+class DAO
+{
 
   // Properties
   private static $dbHost = "localhost";
-	private static $dbName = "todos";
-	private static $dbUser = "todos";
-	private static $dbPass = "todos";
-	private static $sharedPDO;
-	protected $pdo;
+  private static $dbName = "eventsDB";
+  private static $dbUser = "eventsDBPass";
+  private static $dbPass = "eventsDBPass";
+  private static $sharedPDO;
+  protected $pdo;
 
   // Constructor
-	function __construct() {
+  function __construct()
+  {
 
-		if(empty(self::$sharedPDO)) {
-			self::$sharedPDO = new PDO("mysql:host=" . self::$dbHost . ";dbname=" . self::$dbName, self::$dbUser, self::$dbPass);
-			self::$sharedPDO->exec("SET CHARACTER SET utf8");
-			self::$sharedPDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-			self::$sharedPDO->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-		}
+    if (empty(self::$sharedPDO)) {
+      self::$sharedPDO = new PDO("mysql:host=" . self::$dbHost . ";dbname=" . self::$dbName, self::$dbUser, self::$dbPass);
+      self::$sharedPDO->exec("SET CHARACTER SET utf8");
+      self::$sharedPDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      self::$sharedPDO->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+    }
 
-		$this->pdo =& self::$sharedPDO;
-
-	}
+    $this->pdo = &self::$sharedPDO;
+  }
 
   // Methods
 
 }
-
- ?>
